6aba17bba64a627c6e5c4600c32d64edf55816ec
Raphael Lima <raphalima2204@gmail.com>
2026-02-25 16:43:35 -0300

    hello

---
+++ conta-bancaria/src/main/java/com/senai/conta_bancaria/domain/entity/Cliente.java
@@ -0,0 +1,6 @@
+package com.senai.conta_bancaria.domain.entity;
+
+public class Cliente extends Usuario {
+    private Conta conta;
+
+}
---
+++ conta-bancaria/src/main/java/com/senai/conta_bancaria/domain/entity/Gerente.java
@@ -0,0 +1,5 @@
+package com.senai.conta_bancaria.domain.entity;
+
+public class Gerente extends Usuario{
+
+}
