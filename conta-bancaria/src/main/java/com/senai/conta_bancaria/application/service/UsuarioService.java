b9d08488cc4bd4fb50f1d9d69593e63316888588
Raphael Lima <raphalima2204@gmail.com>
2026-03-11 16:36:07 -0300

    usamos postman

--- conta-bancaria/src/main/java/com/senai/conta_bancaria/application/service/UsuarioService.java
+++ conta-bancaria/src/main/java/com/senai/conta_bancaria/application/service/UsuarioService.java
@@ -5,6 +5,8 @@ import com.senai.conta_bancaria.domain.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 //@servic avisa o spring que ele vai controlar
 @Service
 public class UsuarioService {
@@ -15,5 +17,9 @@ public class UsuarioService {
     public Usuario cadastrarUsuario(Usuario usuario) {
 
         return usuarioRepository.save(usuario);
+    }
+
+    public List<Usuario> listarUsuarios() {
+        return usuarioRepository.findAll();
     }
 }
